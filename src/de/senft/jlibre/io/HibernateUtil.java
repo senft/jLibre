d5d66dd947938e4249a4ab4c9d3e1a8c5d3329e5
senft-desktop <senftt@gmail.com>
2012-11-20 00:58:40 +0100

    Added hibernate libs.

---
+++ src/de/senft/jlibre/io/HibernateUtil.java
@@ -0,0 +1,24 @@
+package de.senft.jlibre.io;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+
+	public static SessionFactory getSessionFactory()
+			throws HibernateException {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+	}
+}
