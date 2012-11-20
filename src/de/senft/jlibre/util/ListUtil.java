056383c4cbb20e19fbd7ec17aba419f776103c9f
senft-desktop <senftt@gmail.com>
2012-11-20 20:14:19 +0100

    Added ListUtil, for casting of genericsless lists

---
+++ src/de/senft/jlibre/util/ListUtil.java
@@ -0,0 +1,15 @@
+package de.senft.jlibre.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ListUtil {
+	public static <T> List<T> castList(Class<? extends T> clazz, Collection<?> c) {
+		List<T> r = new ArrayList<T>(c.size());
+		for (Object o : c)
+			r.add(clazz.cast(o));
+		return r;
+	}
+
+}
