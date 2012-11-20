4918244956f633d4aecca1d71ac601b0e9ef76e9
senft-desktop <senftt@gmail.com>
2012-11-20 20:13:30 +0100

    Moved Main out of its package

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
-package de.senft.jlibre.main;
-
 import java.awt.EventQueue;
 
-import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import de.senft.jlibre.gui.MainWindow;
 
 public class Main {
 
-	private static Logger logger = Logger.getLogger(Main.class);
-
-	private MainWindow mainWindow;
-
 	public Main() {
-		logger.info("Started application...");
-
-		mainWindow = new MainWindow();
+		MainWindow mainWindow = new MainWindow();
 		mainWindow.setVisible(true);
-		logger.info("Succesfully initialized all vital components...");
 	}
 
 	public static void main(String args[]) {
@@ -33,7 +23,5 @@ public class Main {
 				}
 			}
 		});
-
 	}
-
 }
